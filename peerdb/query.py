d61b48eaae6c435254c74c380da4fbf36ae529ec
Mitar <mitar.git@tnode.com>
2014-12-10 15:53:07 -0800

    Limit fields.

--- peerdb/query.py
+++ peerdb/query.py
@@ -19,18 +19,22 @@ start = time.time()
 print "Querying all posts and content for each tag"
 
 # iterate over all tags
-for tag in tags.find():
+for tag in tags.find({}, {'name': 1}):
+	# we pretend that tag name is the only thing we have to begin with
+	tag_name = tag['name']
+
 	# query to get posts that include current tag
 	# making sure I got all info for each post
 	# make a list of dicts where each dict contains post contents
 	tp_contents = []
-	for post in posts.find({'tags.name': tag['name']}):
+	for post in posts.find({'tags.name': tag_name}, {'body': 1, 'comments.body': 1, 'author.name': 1, 'author.picture': 1, 'tags.name': 1, 'tags.description' : 1}):
 		tp_contents.append({
 			"body": post['body'],
 			"comments": [comment['body'] for comment in post['comments']],
 			"author_name": post['author']['name'],
-			"author_pic": post['author']['picture'],
-			"tags": [tag['name'] for tag in post['tags']],
+			"author_picture": post['author']['picture'],
+			"tags_name": [tag['name'] for tag in post['tags']],
+			"tags_description": [tag['description'] for tag in post['tags']],
 			})
 
 print "Finished:", "elapsed time", time.time()-start
