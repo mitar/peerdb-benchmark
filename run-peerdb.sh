547ffc48a9de6e01c962de90e1f2cffc34a76d4d
Mitar <mitar.git@tnode.com>
2014-12-15 13:27:29 +0100

    Fix.

--- run-peerdb.sh
+++ run-peerdb.sh
@@ -8,7 +8,7 @@ NODE=$(find /root/.meteor/ -path '*bin/node')
 
 export NODE_ENV="PRODUCTION"
 export MONGO_URL="mongodb://localhost:27017/meteor"
-export MONGO_URL="mongodb://localhost:27017/local"
+export MONGO_OPLOG_URL="mongodb://localhost:27017/local"
 
 if [[ -z "$1" ]]; then
 	export PEERDB_INSTANCES="1"
@@ -17,5 +17,5 @@ else
 fi
 
 for I in $(seq 1 $PEERDB_INSTANCES); do
-  PEERDB_INSTANCE="$((I-1))" PORT="$((2900 + 100 * I))" $NODE main.js &
+  PEERDB_INSTANCE="$((I-1))" PORT="$((2900 + 100 * I))" "$NODE" main.js &
 done
