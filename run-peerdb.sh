18984905c807fda0ee8ff0e72ff6de71d5ea27e4
Mitar <mitar.git@tnode.com>
2014-12-15 15:25:47 +0100

    Use 127.0.0.1 instead of localhost.

--- run-peerdb.sh
+++ run-peerdb.sh
@@ -8,8 +8,8 @@ NODE=$(find /root/.meteor/ -path '*bin/node')
 
 export NODE_PATH="$(dirname $(dirname "$NODE"))/lib/node_modules"
 export NODE_ENV="PRODUCTION"
-export MONGO_URL="mongodb://localhost:27017/meteor"
-export MONGO_OPLOG_URL="mongodb://localhost:27017/local"
+export MONGO_URL="mongodb://127.0.0.1:27017/meteor"
+export MONGO_OPLOG_URL="mongodb://127.0.0.1:27017/local"
 
 if [[ -z "$1" ]]; then
 	export PEERDB_INSTANCES="1"
@@ -18,5 +18,5 @@ else
 fi
 
 for I in $(seq 1 $PEERDB_INSTANCES); do
-  PEERDB_INSTANCE="$((I-1))" PORT="$((2900 + 100 * I))" ROOT_URL="http://localhost:$((2900 + 100 * I))" "$NODE" main.js &
+  PEERDB_INSTANCE="$((I-1))" PORT="$((2900 + 100 * I))" ROOT_URL="http://127.0.0.1:$((2900 + 100 * I))" "$NODE" main.js &
 done
